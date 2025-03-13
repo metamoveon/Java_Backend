2334429d40c8440c3c213ab21717d37f19ba986b
Mr.Sura Polpanich <100360830+metamoveon@users.noreply.github.com>
2025-03-13 08:39:33 +0000

    medianofarray

---
+++ MedianOfArray.java
@@ -0,0 +1,31 @@
+    import java.util.Arrays;
+    
+    public class MedianOfArray {
+    
+        void main() {
+            int[] example1 = {5, 7, 8, 6, 3}; // Expect: 6
+            System.out.println("Result: "+findMedian(example1));
+            int[] example2 = {5, 9, 8, 6, 3, 10}; // Expect: 5.5
+            System.out.println("Result: "+findMedian(example2));
+            int[] example3 = {5};// Expect: 5
+            System.out.println("Result: "+findMedian(example3));
+            int[] example4 = new int[]{}; // Expect: 0
+            System.out.println("Result: "+findMedian(example4));
+        }
+    
+        private static double findMedian(int[] arr) {
+            int length = arr.length;
+            if (length == 0) {
+                return 0;
+            }
+            if (length == 1) {
+                return arr[0];
+            }
+            Arrays.sort(arr);
+            boolean isEven = length % 2 == 0;
+            if (isEven) {
+                return (arr[length / 2] * 0.5) + (arr[(length / 2) - 1] * 0.5);
+            }
+            return arr[length / 2];
+        }
+    }
